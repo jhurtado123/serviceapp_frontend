25c09b7f91347a60eb7a968ea9ff3bab56f2bf4d
Jaime <jaimemoravallejo@gmail.com>
2020-06-03 21:08:34 +0200

    Fix date format

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -73,10 +73,10 @@ class ChatMessage extends Component {
   getYYYMMDDHHMMDate(date) {
     if (typeof date === 'string') date = new Date(date);
     return `${
-      (date.getMonth()+1).toString().padStart(2, '0')}/${
       date.getDate().toString().padStart(2, '0')}/${
+      (date.getMonth() + 1).toString().padStart(2, '0')}/${
       date.getFullYear().toString().padStart(4, '0')} ${
-      date.getHours().toString().padStart(2, '0')}:${
+      (date.getHours()).toString().padStart(2, '0')}:${
       date.getMinutes().toString().padStart(2, '0')}`
   }
 
