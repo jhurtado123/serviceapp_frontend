7bce8b2d570a4c404495df643c9e9b2b5bcedb95
Jose Hurtado <hurtadojose89@gmail.com>
2020-06-05 00:37:21 +0200

    [dev] - test

--- src/components/VideoCall.js
+++ src/components/VideoCall.js
@@ -96,10 +96,10 @@ class VideoCall extends Component {
             urls: "stun:stun.l.google.com:19302",
           },
           {
-            urls: "turn:numb.viagenie.ca",
-            credential: 'muazkh',
-            username: 'webrtc@live.com'
-          }
+            url: 'turn:192.158.29.39:3478?transport=tcp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
+          },
         ]
       },
       stream: stream,
