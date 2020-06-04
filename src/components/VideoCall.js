934d8b2b34a41aabe28d020d62b74ce7e7fb4ca0
Jose Hurtado <hurtadojose89@gmail.com>
2020-06-04 23:46:05 +0200

    [dev]- test

--- src/components/VideoCall.js
+++ src/components/VideoCall.js
@@ -93,15 +93,11 @@ class VideoCall extends Component {
       config: {
 
         iceServers: [
-          {
-            urls: "stun:numb.viagenie.ca",
-            username: "sultan1640@gmail.com",
-            credential: "98376683"
-          },
+
           {
             urls: "turn:numb.viagenie.ca",
-            username: "sultan1640@gmail.com",
-            credential: "98376683"
+            credential: 'muazkh',
+            username: 'webrtc@live.com'
           }
         ]
       },
